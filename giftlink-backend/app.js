893f4d638bc610b599224572e3d67d30b4db9e00
jzhao401 <zhao401@gmail.com>
2025-12-02 11:53:49 -0600

    added searchroute calling

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -37,7 +37,7 @@ app.use(pinoHttp({ logger }));
 app.use("/gift", giftroutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use("/search", searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
