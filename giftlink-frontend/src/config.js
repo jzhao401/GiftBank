9e49cc4e554336195ea8abd2682ccc330c0ab3b6
jzhao401 <zhao401@gmail.com>
2025-12-05 11:31:50 -0600

    changed envs paths

--- giftlink-frontend/src/config.js
+++ giftlink-frontend/src/config.js
@@ -1,7 +1,7 @@
-require('dotenv').config('./envs');
+require("dotenv").config({ path: "./envs" });
 const config = {
   backendUrl: process.env.REACT_APP_BACKEND_URL,
 };
 
-console.log(`backendUrl in config.js: ${config.backendUrl}`)
-export { config as urlConfig }
+console.log(`backendUrl in config.js: ${config.backendUrl}`);
+export { config as urlConfig };
